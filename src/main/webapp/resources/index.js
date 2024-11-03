1f82025c58c2110571d413980248c08704dcec36
Vladimir Praded <73970637+vovapraded@users.noreply.github.com>
2024-11-03 22:51:00 +0300

    complete task

---
+++ src/main/webapp/resources/index.js
@@ -0,0 +1,18 @@
+// Функция для обновления текущей даты и времени
+
+function updateClock() {
+    const now = new Date();
+    const date = now.toLocaleDateString('ru-RU'); // форматирует дату
+    const time = now.toLocaleTimeString('ru-RU'); // форматирует время
+
+    // Отображаем дату и время в элементе с id="clock"
+    document.getElementById('clock').textContent = `${date} ${time}`;
+}
+
+
+    // Обновляем часы один раз при загрузке страницы
+    updateClock();
+// Запускаем обновление каждые 8 секунд (8000 миллисекунд)
+    setInterval(updateClock, 8000);
+
+
